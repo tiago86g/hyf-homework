7567db65e71a2d07ecd1c29a46a44ed8a7f933e3
tiago <tiago86g@gmail.com>
2020-02-08 11:05:50 +0100

    hw database w2

---
+++ databases/week2/School database_DB_w2.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE school;
+USE school;
+
+-- Create a new database containing the following tables:
+	-- Class: with the columns: id, name, begins (date), ends (date)
+CREATE TABLE `class` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (180) NOT NULL,
+`begins` DATETIME NOT NULL,
+`ends` DATETIME NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+	-- Student: with the columns: id, name, email, phone, class_id (foreign key)
+CREATE TABLE `student`(
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` varchar(255) NOT NULL,
+`email` varchar(255) NOT NULL,
+`phone` varchar(255) NULL,
+`class_id` int(10) unsigned NOT NULL,
+CONSTRAINT `fk_class_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- students
+INSERT INTO student ('id', 'name', 'email', 'phone', 'class_id') VALUES(1, 'Tiago Gonçalves', 'tiago86g@gmail.com', 42594575, 1);
+INSERT INTO student('id', 'name', 'email', 'phone', 'class_id') VALUES(2, 'Afrouz Hakimzadeh', 'afrouz@gmail.com', 12345678, 2);
+INSERT INTO student('id', 'name', 'email', 'phone', 'class_id') VALUES(3, 'Ghofrane', 'ghofrane@gmail.com', 87654321, 1);
+INSERT INTO student('id', 'name', 'email', 'phone', 'class_id') VALUES(4, 'Bengü Mochi Özakıncı', 'bengu@gmail.com', 24348746, 3);
+INSERT INTO student('id', 'name', 'email', 'phone', 'class_id') VALUES(5, 'Luciana Caruso', 'LUCH@gmail.com', 79848746, 2);
+
+-- classes
+INSERT INTO class('id', 'name', 'begins', 'ends') VALUES(1, 'frontend', '2020-02-01 00:00:00', '2020-07-31 20:59:00');
+INSERT INTO class('id', 'name', 'begins', 'ends') VALUES(2, 'backend', '2019-10-01 00:00:00', '2020-06-31 20:59:00');
+INSERT INTO class('id', 'name', 'begins', 'ends') VALUES(3, 'final project', '2019-06-01 00:00:00', '2020-04-31 20:59:00');
+
+-- If you are done with the above tasks, you can continue with these advanced tasks:
+	-- Create an index on the name column of the student table.
+CREATE INDEX index_name
+ON student (name);
+    
+	-- Add a new column to the class table named status which can only have the following values: 
+    -- not-started, ongoing, finished (hint: enumerations).
+ALTER TABLE class ADD COLUMN status ENUM ('not-started', 'ongoing', 'finished'); 
+
+
