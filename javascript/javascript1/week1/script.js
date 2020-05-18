874ae38c9c79d18e2a3735cc468d2ba2a39a7b4b
tiago <tiago86g@gmail.com>
2020-05-18 14:49:41 +0200

    updating JS1 week 1 homework

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,73 +1,91 @@
-// Age-ify (A future age calculator)
-
+/////////////// 1 - Age-ify (A future age calculator)
 const yearOfBirth = 1986;
 const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}`);
 
-console.log(`You will be ${age} years old in ${yearFuture}`); 
-    
-
-  
-
-// Goodboy-Oldboy (A dog age calculator)
-
-let dogYearOfBirth = 2016;
-let dogYearFuture = 2027;
+/////////////// 2 - Goodboy-Oldboy (A dog age calculator)
+const dogYearOfBirth = 2014;
+const dogYearFuture = 2027;
 let dogYear = dogYearFuture - dogYearOfBirth;
 let shouldShowResultInDogYears = true;
-    
-if (shouldShowResultInDogYears){
-futureAgeInDogYears = dogYear * 7;
-console.log(`Your dog will be ${futureAgeInDogYears} dog years old in 2027`);
+
+if ((shouldShowResultInDogYears = true)) {
+  dogYear = dogYear * 7;
+  console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
 } else {
-    console.log(`Your dog will be ${dogYear} human years old in 2027`);
-}
-  
-
-    
-
-// Housey pricey (A house price estimator)
-
-let widePeter = 8;
-let deepPeter = 10;
-let highPeter = 10;
-let houseSizeInM3Peter = (widePeter * deepPeter * highPeter)
-let gardenSizeInM2Peter = 100;
-let houseOfferPeter = 2500000;
-
-let housePricePeter = houseSizeInM3Peter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
-console.log(`Peter should pay ${housePricePeter}`);
-
-if(houseOfferPeter<housePricePeter){
-    console.log("Peter is makinig a good business!");
-} else {
-    console.log("Peter is paying too much...");
+  console.log(
+    `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
+  );
 }
 
+/////////////// 3 - Housey pricey (A house price estimator)
+const volumeInMetersPeter = 8 * 10 * 10;
+const gardenSizeInM2Peter = 100;
+const houseOfferPeter = 2500000;
+let housePricePeter =
+  volumeInMetersPeter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
 
-const wideJulia = 5;
-const deepJulia = 11;
-const highJulia = 8;
-const houseSizeInM3Julia = (wideJulia * deepJulia * highJulia)
+let priceEvaluationPeter;
+if (housePricePeter < houseOfferPeter) {
+  priceEvaluationPeter = 'Peter is paying too much';
+} else {
+  priceEvaluationPeter = 'Peter is paying too little';
+}
+console.log(priceEvaluationPeter);
+
+console.log(
+  `Peter's house value is ${housePricePeter} and Peter is paying ${houseOfferPeter}`
+);
+
+const volumeInMetersJulia = 5 * 11 * 8;
 const gardenSizeInM2Julia = 70;
-const houseOfferJulia =  1000000;
+const houseOfferJulia = 100000;
+let housePriceJulia =
+  volumeInMetersJulia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
 
-let housePriceJulia = houseSizeInM3Julia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
-console.log(`Julia should pay ${housePriceJulia}`);   
-
-if(houseOfferJulia<housePriceJulia){
-    console.log("Julia is makinig a good business!");
+let priceEvaluationJulia;
+if (housePriceJulia < houseOfferJulia) {
+  priceEvaluationJulia = 'Julia is paying too much';
 } else {
-    console.log("Julia is paying too much...");
+  priceEvaluationJulia = 'Julia is paying too little';
 }
+console.log(priceEvaluationJulia);
 
+console.log(
+  `Julia's house value is ${housePriceJulia} and Julia is paying ${houseOfferJulia}`
+);
 
+/////////////// 4 - Ez Namey (Startup name generator) Optional
+const firstWords = [
+  'Easy',
+  'Awesome',
+  'Corporate',
+  'Crazy',
+  'Funny',
+  'Smart',
+  'Super',
+  'The',
+  'Brave',
+  'True'
+];
 
+const secondWords = [
+  'Startup',
+  'Corporation',
+  'Company',
+  'Frinsds',
+  'Design',
+  'Code',
+  'Brothers',
+  'Sisters',
+  'Coffee',
+  'Food'
+];
 
-//Ez Namey (Startup name generator) Optional
+let firstRandomWords = firstWords[Math.floor(Math.random() * 10) + 0];
+let secondRamdomWords = secondWords[Math.floor(Math.random() * 10) + 0];
 
-let firstWords = ["Easy", "Awesome", "Corporate", "Crazy", "Amazing", "Super", "Blue", "Green", "Fantastic", "Tender"];
-let secondWords = ["Corporate", "Travel", "Life", "Love", "Connection", "Car", "Food", "Friend", "Toy", "Help" ]
-let startupName = [(firstWords[Math.floor(Math.random() * 10) + 0]) + " " + secondWords[Math.floor(Math.random() * 10) + 0]];
-
-console.log("Your startup name will be... " + startupName);
+console.log(
+  `Your new stsrtup name is ${firstRandomWords} ${secondRamdomWords}`
+);
