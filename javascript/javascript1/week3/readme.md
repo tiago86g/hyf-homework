fbdad1b67122d26088f642bc384238dbf198bae8
tiago <tiago86g@gmail.com>
2020-06-03 22:39:36 +0200

    JS1_w3 exercise 3

--- javascript/javascript1/week3/readme.md
+++ javascript/javascript1/week3/readme.md
@@ -8,3 +8,14 @@
    Write a function where you speficy your speed in km/h and how far you have to go in km. The function has to return the time it will take to arrive at your destination. The time should be formatted like this: 3 hours and 34 minutes.
 
 Hint: For formatting your best friend is Google!
+
+3. Series duration of my life
+   How much time of my life have i been watching series on tv? Lets find out! Calculate how much time a tv series have taken as a percentage of an average lifespan of 80 years.
+   Firstly change the seriesDurations array found above to include your favorite series. Find the duration of a series here: http://tiii.me/
+   Create a function that logs out the following text using the seriesDurations array:
+
+Game of thrones took 0.01% of my life
+Sopranos took 0.012% of my life
+The Wire took 0.007% of my life
+
+In total that is 0.2% of my life
--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -30,3 +30,56 @@ function timeToTrave() {
 }
 const travelTime = timeToTrave(travelInformation);
 console.log(travelTime); // 4 hours and 42 minutes
+
+// 3. Series duration of my life
+const seriesDurations = [
+  {
+    title: 'Game of thrones',
+    days: 3,
+    hours: 1,
+    minutes: 0
+  },
+  {
+    title: 'Sopranos',
+    days: 3,
+    hours: 14,
+    minutes: 0
+  },
+  {
+    title: 'The Wire',
+    days: 2,
+    hours: 12,
+    minutes: 0
+  }
+];
+function addSerie(serie) {
+  seriesDurations.push(serie);
+}
+
+addSerie({
+  title: 'Black Mirror',
+  days: 0,
+  hours: 19,
+  minutes: 15
+});
+addSerie({
+  title: '3%',
+  days: 0,
+  hours: 19,
+  minutes: 30
+});
+
+function totalSeriesInLife(list) {
+  const lifeInMinutes = 80 * 525948.766;
+  list.map((serie, index) => {
+    const totalSerieInMinutes =
+      serie.minutes + serie.hours * 60 + serie.days * 1440;
+    const percentageOfLife =
+      (totalSerieInMinutes / lifeInMinutes - 1) * 100 + 100;
+
+    console.log(
+      `${serie.title} took ${percentageOfLife.toFixed(3)}% of my life`
+    );
+  });
+}
+totalSeriesInLife(seriesDurations);
