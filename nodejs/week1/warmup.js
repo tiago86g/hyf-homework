8d7c15f5aba68c31cd57f1cbc40233157121ef96
tiago <tiago86g@gmail.com>
2020-06-10 14:52:01 +0200

    NodeJS_w1 implementing Aleks review

--- nodejs/week1/warmup.js
+++ nodejs/week1/warmup.js
@@ -1,32 +1,24 @@
 class Circle {
-    constructor(radius) {
-        this.radius = radius;
-    }
-
-    getDiameter() {
-        this.diameter = 2 * this.radius;
-        console.log(`The diameter is ${this.diameter}`);
-    }
-
-    getCircumference() {
-
-        this.circumference = 2 * Math.PI * this.radius;
-        console.log(`The circumference is ${this.circumference.toFixed(1)}`);
-    }
-
-    getArea() {
-        this.area = Math.PI * Math.pow(this.radius, 2);
-        console.log(`The area is ${this.area.toFixed(1)}`);
-
-    }
+  constructor(radius) {
+    this.radius = radius;
+  }
+  getDiameter() {
+    return 2 * this.radius;
+  }
+  getCircumference() {
+    return (2 * Math.PI * this.radius).toFixed(1);
+  }
+  getArea() {
+    return (Math.PI * Math.pow(this.radius, 2)).toFixed(1);
+  }
 }
 
 const circle = new Circle(2);
-circle.getDiameter();
-circle.getCircumference();
-circle.getArea();
+console.log(`The diameter is ${circle.getDiameter()}`);
+console.log(`The circumference is ${circle.getCircumference()}`);
+console.log(`The area is ${circle.getArea()}`);
 
 const circle2 = new Circle(4);
-circle2.getDiameter();
-circle2.getCircumference();
-circle2.getArea();
+console.log(`The diameter is ${circle2.getDiameter()}`);
+console.log(`The circumference is ${circle2.getCircumference()}`);
+console.log(`The area is ${circle2.getArea()}`);
