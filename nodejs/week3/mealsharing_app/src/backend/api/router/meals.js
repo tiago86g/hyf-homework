73c354422795f782cc68862093cc08025d83ff18
tiago <tiago86g@gmail.com>
2020-07-02 16:30:26 +0200

    available reservation feature

--- nodejs/week3/mealsharing_app/src/backend/api/router/meals.js
+++ nodejs/week3/mealsharing_app/src/backend/api/router/meals.js
@@ -3,8 +3,68 @@ const router = express.Router();
 const knex = require('../../database');
 
 router.get('/', async (req, res) => {
-  const meals = await knex('meal');
-  res.json(meals);
+  try {
+    const {
+      maxPrice,
+      availableReservations,
+      title,
+      createAfter,
+      limit
+    } = req.query;
+    if (
+      !maxPrice &&
+      !availableReservations &&
+      !title &&
+      !createAfter &&
+      !limit
+    ) {
+      const meals = await knex('meal');
+      res.json(meals);
+    } else if (maxPrice) {
+      const meals = await knex('meal').where('price', '<', maxPrice);
+      res.send(meals);
+    } else if (title) {
+      const meals = await knex('meal')
+        .where('title', 'like', `%${title}%`)
+        .toSQL()
+        .toNative();
+      res.send(meals);
+    } else if (availableReservations == 'true') {
+      meals = await knex('Meal')
+        .select(
+          knex.raw(
+            `Meal.id, Meal.title, Meal.max_reservations, SUM(reservation.number_of_guests) AS total_guests`
+          )
+        )
+        .innerJoin('reservation', 'meal.id', 'reservation.meal_id')
+        .groupBy('Meal.id')
+        .having(knex.raw(`total_guests < Meal.max_reservations`))
+        .catch(e => {
+          console.error(e);
+        });
+      // meals = await knex.raw(
+      //   `
+      // SELECT Meal.id, Meal.title, Meal.max_reservations,
+      // SUM(reservation.number_of_guests) AS total_guests
+      // FROM Meal
+      // INNER JOIN Reservation ON Reservation.meal_id = Meal.id
+      // GROUP BY Meal.id
+      // HAVING total_guests < Meal.max_reservations`
+      // );
+      // meals = await knex('Meal')
+      //   .select('Meal.id', 'Meal.title', 'Meal.max_reservations')
+      //   .sum({ totalGuests: 'reservation.number_of_guests' })
+      //   .innerJoin('reservation', 'meal.id', 'reservation.meal_id')
+      //   .groupBy('Meal.id')
+      //   .having('totalGuests', '<', 'Meal.max_reservations')
+      //   .catch(e => {
+      //     console.error(e);
+      //   });
+      console.log('meals', meals);
+      res.send(meals);
+    }
+    console.log(availableReservations);
+  } catch (error) {}
 });
 
 router.get('/:id', async (req, res) => {
