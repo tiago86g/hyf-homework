112aee9c390082177e591840167d58c63ed157be
tiago <tiago86g@gmail.com>
2020-08-25 23:47:32 +0200

    edit feature

--- react/week3/todolist/src/App.js
+++ react/week3/todolist/src/App.js
@@ -4,10 +4,71 @@ import { Counter } from './Counter';
 import { TodoForm } from './TodoForm';
 
 export class App extends Component {
-  state = {
-    todosArray: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      todosArray: [
+        {
+          id: 1,
+          description: 'Get out of bed',
+          deadline: '2019-06-14',
+          completed: false,
+          isEditing: false
+        },
+        {
+          id: 2,
+          description: 'Brush teeth',
+          deadline: '2019-07-14',
+          completed: false,
+          isEditing: false
+        },
+        {
+          id: 3,
+          description: 'Eat breakfast',
+          deadline: '2019-07-14',
+          completed: false,
+          isEditing: false
+        }
+      ]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id, event) {
+    const todos = this.state.todosArray.map(todo => {
+      if (id === todo.id) {
+        return { ...todo, [event.target.id]: event.target.value };
+      }
+      return todo;
+    });
+    this.setState({ todos });
+  }
+  editTodo = (id, isEditingMode) => {
+    // if (isEditingMode) {
+    //   const todosArray = this.state.todosArray.map(todo => {
+    //     if (id === todo.id) {
+    //       return {
+    //         ...todo,
+    //         editing: false
+    //       };
+    //     }
+    //     return todo;
+    //   });
+    //   this.setState({ todosArray });
+    // } else {
+    const todosArray = this.state.todosArray.map(todo => {
+      if (id === todo.id) {
+        return {
+          ...todo,
+          isEditing: true
+        };
+      }
+      return todo;
+    });
+    this.setState({ todosArray });
+    //}
   };
-  editTodo = id => {};
 
   deleteTodo = id => {
     const todosArray = this.state.todosArray.filter(todo => {
@@ -34,13 +95,13 @@ export class App extends Component {
     this.setState({ todosArray });
   };
 
-  componentDidMount() {
-    fetch(
-      'https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw'
-    )
-      .then(response => response.json())
-      .then(data => this.setState({ todosArray: data }));
-  }
+  // componentDidMount() {
+  //   fetch(
+  //     'https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw'
+  //   )
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ todosArray: data }));
+  // }
 
   render() {
     return (
@@ -52,6 +113,7 @@ export class App extends Component {
           deleteTodo={this.deleteTodo}
           completeTodo={this.completeTodo}
           editTodo={this.editTodo}
+          handleChange={this.handleChange}
         />
       </div>
     );
--- react/week3/todolist/src/TodoList.js
+++ react/week3/todolist/src/TodoList.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 export function TodoList(props) {
   const { todosArray } = props;
-  const { deleteTodo, completeTodo, editTodo } = props;
+  const { deleteTodo, completeTodo, editTodo, handleChange } = props;
+
   const length = todosArray.length;
   const mappedTodos = length ? (
     todosArray.map(todo => {
@@ -15,27 +16,56 @@ export function TodoList(props) {
             padding: '4px 4px',
             marginBottom: '2px'
           }}>
-          <span
-            style={{
-              textDecorationLine: todo.completed ? 'line-through' : 'none',
-              fontWeight: 'bold'
-            }}
-            id={todo.id}>
-            {todo.description} >
-          </span>
-          <span
-            style={{
-              textDecorationLine: todo.completed ? 'line-through' : 'none'
-            }}
-            id={todo.id}>
-            >> {todo.deadline}
-          </span>
-          <input
-            type='checkbox'
-            checked={todo.completed ? 'checked' : ''}
-            onChange={() => completeTodo(todo.id)}></input>
-          <button onClick={() => deleteTodo(todo.id)}>delete task</button>
-          <button onClick={() => editTodo(todo.id)}>edit</button>
+          {todo.isEditing ? (
+            <form>
+              <input
+                type='text'
+                id='description'
+                placeholder='write your task here'
+                onChange={event => handleChange(todo.id, event)}
+                // value={todo.description}
+                required></input>
+
+              <input
+                type='date'
+                id='deadline'
+                min={new Date().toISOString().slice(0, 10)}
+                max='2020-12-31'
+                onChange={event => handleChange(todo.id, event)}
+                // value={todo.deadline}
+                required></input>
+
+              <button onClick={() => {}}>done</button>
+            </form>
+          ) : (
+            <div>
+              <input
+                type='checkbox'
+                checked={todo.completed ? 'checked' : ''}
+                onChange={() => completeTodo(todo.id)}
+              />
+
+              <span
+                style={{
+                  textDecorationLine: todo.completed ? 'line-through' : 'none',
+                  fontWeight: 'bold'
+                }}
+                id={todo.id}>
+                {`${todo.description} `}
+              </span>
+
+              <span
+                style={{
+                  textDecorationLine: todo.completed ? 'line-through' : 'none'
+                }}
+                id={todo.id}>
+                >> {todo.deadline}
+              </span>
+
+              <button onClick={() => deleteTodo(todo.id)}>delete task</button>
+              <button onClick={() => editTodo(todo.id)}>edit</button>
+            </div>
+          )}
         </div>
       );
     })
