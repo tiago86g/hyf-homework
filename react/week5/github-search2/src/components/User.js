af3bececf75d4d838c3a4ad0142372527d0b32fc
tiago <tiago86g@gmail.com>
2020-09-03 13:17:06 +0200

    user and HYT repo features

--- react/week5/github-search2/src/components/User.js
+++ react/week5/github-search2/src/components/User.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function User() {
+export default function User({ match }) {
+  // console.log({ match });
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `https://api.github.com/users/${match.params.id}`;
+        const matchedUser = await fetch(url).then(res => res.json());
+        setUser(matchedUser); // connect "match" with the "user" useState
+      } catch (err) {
+        return console.error();
+      }
+    };
+    fetchData();
+  }, [match]);
+  console.log({ match });
+  console.log({ user });
+
+  if (!user.login) {
     return (
-        <div>
-            The user
-        </div>
-    )
+      <div>
+        <p>User not found!</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>GitHub user</h1>
+        <img src={user.avatar_url} alt={user.login} style={{ maxWidth: 300 }} />
+        <p>
+          Name: <strong>{user.name || 'not available'}</strong>
+        </p>
+        <p>
+          Username: <strong>{user.login || 'not available'}</strong>
+        </p>
+        <p>
+          E-mail: <strong>{user.email || 'not available'}</strong>
+        </p>
+        <p>
+          Location: <strong>{user.location || 'not available'}</strong>
+        </p>
+        <p>
+          Company: <strong>{user.company || 'not available'}</strong>
+        </p>
+        <p>
+          Created at:{' '}
+          <strong>{user.created_at.slice(0, 10) || 'not available'}</strong>
+        </p>
+        <p>
+          Following: <strong>{user.following || 'not available'}</strong>
+        </p>
+        <p>
+          Followers: <strong>{user.followers || 'not available'}</strong>
+        </p>
+        <p>
+          Type: <strong>{user.type || 'not available'}</strong>
+        </p>
+        <p>
+          Twitter: <strong>{user.twitter_username || 'not available'}</strong>
+        </p>
+        <p>
+          Last update:{' '}
+          <strong>{user.updated_at.slice(0, 10) || 'not available'}</strong>
+        </p>
+        <a href={user.html_url} target={user.html_url}>
+          <button>See on GitHub</button>
+        </a>
+      </div>
+    );
+  }
 }
